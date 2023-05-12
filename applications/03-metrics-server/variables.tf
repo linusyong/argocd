ab7a04c0fb888841a9cec4cad58ac653412c8279
Linus Yong <linusyong@gmail.com>
2023-05-12 11:41:01 +1000

    fix correct helm version for metrics server

--- applications/03-metrics-server/variables.tf
+++ applications/03-metrics-server/variables.tf
@@ -1,13 +1,13 @@
 variable "helm_repo_version" {
   description = "The helm repository version"
   type        = string
-  default     = "v0.0.2"
+  default     = "v0.0.5"
   # https://github.com/linusyong/helm-charts/tags
 }
 
 variable "metrics_server_helm_version" {
   description = "The helm chart version of metrics-server"
   type        = string
-  default     = "6.2.17"
-  # https://artifacthub.io/packages/helm/bitnami/metrics-server
+  default     = "3.10.0"
+  # https://github.com/kubernetes-sigs/metrics-server/tags
 }
