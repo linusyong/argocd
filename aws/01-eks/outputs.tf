2946334a06cd5f9d490efb02adb462cb50d74b40
Linus Yong <linusyong@gmail.com>
2023-05-16 19:16:47 +1000

    updated output for eks cluster and node information

--- aws/01-eks/outputs.tf
+++ aws/01-eks/outputs.tf
@@ -1,3 +1,11 @@
 output "oidc_url" {
   value = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
+
+output "node_role_arn" {
+  value = resource.aws_iam_role.node.arn
+}
+
+output "cluster" {
+  value = resource.aws_eks_cluster.this
+}
