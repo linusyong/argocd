20c1e063efca0e463881f7f6d521be64af0248e0
Linus Yong <linusyong@gmail.com>
2023-05-16 20:10:17 +1000

    updated output

--- aws/01-eks/outputs.tf
+++ aws/01-eks/outputs.tf
@@ -2,8 +2,8 @@ output "oidc_url" {
   value = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
-output "node_role_arn" {
-  value = resource.aws_iam_role.node.arn
+output "node_role" {
+  value = resource.aws_iam_role.node
 }
 
 output "cluster" {
