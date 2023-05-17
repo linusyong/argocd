a1a8de31e57a2ed3ab9301184a491aa799bca4e6
Linus Yong <linusyong@gmail.com>
2023-05-17 10:07:27 +1000

    update to release v0.0.7

--- applications/03-metrics-server/variables.tf
+++ applications/03-metrics-server/variables.tf
@@ -1,7 +1,7 @@
 variable "helm_repo_version" {
   description = "The helm repository version"
   type        = string
-  default     = "v0.0.6"
+  default     = "v0.0.7"
   # https://github.com/linusyong/helm-charts/tags
 }
 
