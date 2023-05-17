1983f4200598f2868d03c6981321ad68a26e85e9
Linus Yong <linusyong@gmail.com>
2023-05-17 19:13:39 +1000

    update repo to release v0.0.10

--- applications/03-metrics-server/variables.tf
+++ applications/03-metrics-server/variables.tf
@@ -1,7 +1,7 @@
 variable "helm_repo_version" {
   description = "The helm repository version"
   type        = string
-  default     = "v0.0.9"
+  default     = "v0.0.10"
   # https://github.com/linusyong/helm-charts/tags
 }
 
--- applications/04-karpenter/variables.tf
+++ applications/04-karpenter/variables.tf
@@ -12,7 +12,7 @@ variable "region" {
 variable "helm_repo_version" {
   description = "The helm repository version"
   type        = string
-  default     = "v0.0.9"
+  default     = "v0.0.10"
   # https://github.com/linusyong/helm-charts/tags
 }
 
